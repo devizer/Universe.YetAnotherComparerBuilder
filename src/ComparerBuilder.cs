2dede257122219ce025ba96c957aa8e498878e77
devizer <devizer@users.github.com>
2019-03-03 06:42:16 +0200

    Adding EqualityComparerBuilder

--- src/ComparerBuilder.cs
+++ src/ComparerBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Universe
@@ -14,7 +15,7 @@ namespace Universe
     }
 
     // Emit-free, fast, strongly typed, highly-customizable yet another ComparerBuilder
-    public class ComparerBuilder<TItem>
+    public partial class ComparerBuilder<TItem>
     {
         private readonly List<FieldMeta> _Comparisons = new List<FieldMeta>();
 
@@ -129,6 +130,88 @@ namespace Universe
             }
         }
     }
+
+
+    // Warning. This class is just a p-o-c. And it couses short-term boxing
+    public class EqualityComparerBuilder<T>
+    {
+        private sealed class EqualityParts
+        {
+            public Func<T, object> Function;
+            public IEqualityComparer EqualityComparer;
+        }
+
+        private sealed class AnEqualityComparer<TItem> : IEqualityComparer
+        {
+            private IEqualityComparer<TItem> EqualityComparer;
+
+            public AnEqualityComparer(IEqualityComparer<TItem> equalityComparer)
+            {
+                EqualityComparer = equalityComparer;
+            }
+
+            public bool Equals(object x, object y)
+            {
+                return this.EqualityComparer.Equals((TItem)x, (TItem)y);
+            }
+
+            public int GetHashCode(object obj)
+            {
+                return this.EqualityComparer.GetHashCode((TItem)obj);
+            }
+        }
+
+        private List<EqualityParts> Parts = new List<EqualityParts>();
+
+        public EqualityComparerBuilder<T> Use<TField>(Func<T, TField> expression, IEqualityComparer<TField> equalityComparer)
+        {
+            Parts.Add(new EqualityParts()
+            {
+                Function = delegate(T arg) { return expression(arg); },
+                EqualityComparer = new AnEqualityComparer<TField>(equalityComparer)
+            });
+
+            return this;
+        }
+
+        public IEqualityComparer<T> GetEqualityComparer()
+        {
+            return new GenericEqualityComparer(Parts);
+        }
+
+        private class GenericEqualityComparer : IEqualityComparer<T>
+        {
+            private List<EqualityParts> Parts;
+
+            public GenericEqualityComparer(List<EqualityParts> parts)
+            {
+                Parts = parts;
+            }
+
+            public bool Equals(T x, T y)
+            {
+                foreach (var part in Parts)
+                {
+                    if (!part.EqualityComparer.Equals(part.Function(x), part.Function(y)))
+                        return false;
+                }
+
+                return true;
+            }
+
+            public int GetHashCode(T arg)
+            {
+                int ret = 31;
+                foreach (var part in Parts)
+                {
+                    ret = (ret * -1521134295) ^ part.EqualityComparer.GetHashCode(part.Function(arg));
+                }
+
+                return ret;
+            }
+        }
+    }
+
 #if CSHARP_OLD
     public delegate TResult Func<T1, T2, TResult>(T1 arg1, T2 arg2);
     public delegate TField Func<TItem, TField>(TItem item);
