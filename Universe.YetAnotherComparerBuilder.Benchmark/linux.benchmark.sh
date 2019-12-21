83bc98c605ee1753569d51a01134088a3ab7b75b
devizer <devizer@users.github.com>
2019-12-21 14:52:27 +0200

    linux benchmarks2

--- Universe.YetAnotherComparerBuilder.Benchmark/linux.benchmark.sh
+++ Universe.YetAnotherComparerBuilder.Benchmark/linux.benchmark.sh
@@ -1,4 +1,5 @@
+#!/usr/bin/env bash
 # work=$HOME/build/devizer; mkdir -p $work; cd $work; git clone https://github.com/devizer/Universe.YetAnotherComparerBuilder; cd Universe.YetAnotherComparerBuilder; git pull; cd Universe.YetAnotherComparerBuilder.Benchmark; bash linux.benchmark.sh
 dotnet build -c Release -v q || true
 msbuild /t:Rebuild /p:Configuration=Release /v:q
-dotnet benchmark bin/Release/net462/Universe.YetAnotherComparerBuilder.Benchmark.dll -r netcoreapp2.2 Mono -filter * -j short
+dotnet benchmark bin/Release/net462/Universe.YetAnotherComparerBuilder.Benchmark.exe -r netcoreapp2.2 Mono -filter * -j short
